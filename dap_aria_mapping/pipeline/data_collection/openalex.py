fda64e2dfc789478fa4556ea37c6cad304ee3b13
Jack-Vines <jack.vines@live.co.uk>
2022-11-30 09:48:36 +0000

    Add first iteration of openalex pipeline

---
+++ dap_aria_mapping/pipeline/data_collection/openalex.py
@@ -0,0 +1,117 @@
+"""
+works pipeline
+--------------
+
+A pipeline that takes a list of concept IDs and years, and outputs OpenAlex API results.
+
+The thought behind this is to break the results into manageable yearly chunks. For a given year
+and high level concept, the output works may be well over 2GB in size when saved to json.
+"""
+import json
+import requests
+from metaflow import FlowSpec, S3, step, Parameter, retry, batch
+
+
+YEARS = [
+    2022,
+    2021,
+    2020,
+    2019,
+    2018,
+    2017,
+    2016,
+    2015,
+    2014,
+    2013,
+    2012,
+    2011,
+    2010,
+    2009,
+    2008,
+    2007
+]
+
+API_ROOT = "https://api.openalex.org/works?filter="
+
+
+def api_generator(api_root: str, year: str) -> list:
+    """Generates a list of all URLs needed to completely collect
+    all works relating to the list of concepts.
+
+    Args:
+        api_root : root URL of the OpenAlex API
+        concept_ids : list of concept IDs to be queried
+
+    Returns:
+        all_pages: list of pages required to return all results
+    """
+    page_one = f"{api_root}institutions.country_code:gb,publication_year:{year}"
+    print(page_one)
+    total_results = requests.get(page_one).json()["meta"]["count"]
+    print(total_results)
+    number_of_pages = -(total_results // -200)  # ceiling division
+    all_pages = [
+        f"{api_root}institutions.country_code:gb,publication_year:{year}&per-page=200&cursor="
+        for _ in range(1, number_of_pages + 1)
+    ]
+    return all_pages
+
+
+class OpenAlexWorksFlow(FlowSpec):
+    production = Parameter("production", help="Run in production?", default=False)
+
+    @step
+    def start(self):
+        """
+        Starts the flow.
+        """
+        self.next(self.generate_api_calls)
+
+    @step
+    def generate_api_calls(self):
+        """Generates all API calls, if test, just one page"""
+        # If production, generate all pages
+        if self.production:
+            self.year_list = YEARS
+        else:
+            self.year_list = YEARS[:1]
+        self.next(self.retrieve_data, foreach="year_list")
+
+    @retry()
+    @batch(cpu=2, memory=48000)
+    @step
+    def retrieve_data(self):
+        """Returns all results of the API hits"""
+        # Get list of API calls
+        api_call_list = api_generator(API_ROOT, self.input)
+        # Get all results
+        outputs = []
+        cursor = "*"  # cursor iteration required to return >10k results
+        for call in api_call_list:
+            try: # catch transient errors
+                req = requests.get(f"{call}{cursor}").json()
+                for result in req["results"]:
+                    outputs.append(result)
+                cursor = req["meta"]["next_cursor"]
+            except:
+                pass
+        # Define a filename and save to S3
+        year = self.input.split(":")[-1] # not ideal for multiple concepts, but works for now
+        # concept_length = self.input.split(",")[0].count("|")
+        filename = f"openalex-works_production-{self.production}_year-{year}.json"
+        with S3(run=self) as s3:
+            data = json.dumps(outputs)
+            s3.put(filename, data)
+        self.next(self.dummy_join)
+
+    @step
+    def dummy_join(self, inputs):
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == "__main__":
+    OpenAlexWorksFlow()

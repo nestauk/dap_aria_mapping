56fc5b662e21666d23013b7519e113a4e5816391
India Kerle <india.kerle@nesta.org.uk>
2022-12-05 17:03:57 +0000

    add unique ids

--- dap_aria_mapping/pipeline/data_collection/tests/test_patents.py
+++ dap_aria_mapping/pipeline/data_collection/tests/test_patents.py
@@ -12,6 +12,14 @@ patents_sample = get_patents().sample(50)
 
 
 def test_patents():
+    # make sure every publication number is unique
+    assert (
+        len(patents_sample) == len(patents_sample["publication_number"].unique())
+    ) == True
+
+    # make sure every family id is unique as we should only have one patent document number per family id
+    assert (len(patents_sample) == len(patents_sample["family_id"].unique())) == True
+
     # make sure the filing date is between 2016-01-01 and 2021-12-31
     assert (
         (patents_sample["filing_date"] < "2021-12-31")

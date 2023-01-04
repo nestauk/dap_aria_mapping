b4dc0af3e5a007ac594ccd474fe5fb0faf34fb7c
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.local>
2023-01-04 07:58:23 -0500

    adding getter for taxonomy dataframe

--- dap_aria_mapping/getters/taxonomies.py
+++ dap_aria_mapping/getters/taxonomies.py
@@ -1,8 +1,24 @@
 from nesta_ds_utils.loading_saving.S3 import download_obj
 from dap_aria_mapping import BUCKET_NAME
-import boto3, yaml, pickle
 import pandas as pd
+import boto3, yaml, pickle
 from toolz import pipe
+
+def get_cooccurrence_taxonomy() -> pd.DataFrame:
+    """gets taxonomy developed using community detection on term cooccurrence network.
+        Algorithm to generate taxonomy can be found in pipeline/taxonomy_development/community_detection.py
+
+    Returns:
+        pd.DataFrame: table describing cluster assignments.
+        Index: entity, Columns: levels of taxonomy, values are expressed as <INT>_<INT> where there is
+        an integer to represent
+    """
+    return download_obj(
+        BUCKET_NAME,
+        "outputs/community_detection_clusters.parquet",
+        download_as="dataframe",
+    )
+
 
 
 def get_taxonomy_config() -> dict:

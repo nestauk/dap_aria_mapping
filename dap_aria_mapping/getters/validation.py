98c6ffef894d81fecf97b5884018acbc7891242b
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2023-01-26 14:21:20 +0000

    adding getters  for distribution statistics

--- dap_aria_mapping/getters/validation.py
+++ dap_aria_mapping/getters/validation.py
@@ -15,3 +15,27 @@ def get_tree_depths(tax_name: str) -> Dict[str, int]:
         BUCKET_NAME,
         'outputs/validation_metrics/taxonomy_depth/{}.json'.format(tax_name),
         download_as = "dict")
+
+def get_entropy() -> dict:
+    """gets a dictionary with metrics about the entropy of the distribution of 
+        entites/category at a given level of the taxonomy
+
+    Returns:
+        dict: key: taxonomy name, value: dict: key: level, value: entropy of frequency distribution
+    """
+    return download_obj(
+        BUCKET_NAME,
+        'outputs/validation_metrics/entropy.json',
+        download_as = "dict")
+
+def get_chisq() -> dict:
+    """gets a dictionary with the chi square test statistic that compares the frequency 
+        distribution of entities/category to a uniform distribution
+
+    Returns:
+        dict: key: taxonomy name, value: dict: key: level, value: chisq stat
+    """
+    return download_obj(
+        BUCKET_NAME,
+        'outputs/validation_metrics/chisq.json',
+        download_as = "dict")

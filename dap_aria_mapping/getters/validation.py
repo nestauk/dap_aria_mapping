95dce76c65c544a3a22cbda169c7cb58298b4a86
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2023-01-31 11:22:10 +0000

    fixing typehints

--- dap_aria_mapping/getters/validation.py
+++ dap_aria_mapping/getters/validation.py
@@ -2,7 +2,7 @@ from nesta_ds_utils.loading_saving.S3 import download_obj
 from dap_aria_mapping import BUCKET_NAME
 from typing import Dict
 
-def get_tree_depths(tax_name: str) -> Dict[str, int]:
+def get_tree_depths(tax_name: str) -> Dict[str, Dict[str, int]]:
     """gets a dictionary with metrics about the tree depth of the taxonomies
 
     Args:
@@ -16,7 +16,7 @@ def get_tree_depths(tax_name: str) -> Dict[str, int]:
         'outputs/validation_metrics/taxonomy_depth/{}.json'.format(tax_name),
         download_as = "dict")
 
-def get_entropy() -> dict:
+def get_entropy() -> Dict[str, int]:
     """gets a dictionary with metrics about the entropy of the distribution of 
         entites/category at a given level of the taxonomy
 
@@ -28,7 +28,7 @@ def get_entropy() -> dict:
         'outputs/validation_metrics/entropy.json',
         download_as = "dict")
 
-def get_chisq() -> dict:
+def get_chisq() -> Dict[str, int]:
     """gets a dictionary with the chi square test statistic that compares the frequency 
         distribution of entities/category to a uniform distribution
 
--- dap_aria_mapping/pipeline/taxonomy_validation/distribution_statistics/chisq.py
+++ dap_aria_mapping/pipeline/taxonomy_validation/distribution_statistics/chisq.py
@@ -6,20 +6,19 @@ from scipy.stats import chisquare
 from nesta_ds_utils.loading_saving.S3 import upload_obj
 from dap_aria_mapping import BUCKET_NAME
 from itertools import repeat
+from typing import Dict
 
-def tax_chisq(tax: pd.DataFrame, tax_name: str, metrics: dict) -> dict:
+def tax_chisq(tax: pd.DataFrame) -> Dict[str, int]:
     """calculates the chi square test statistic of the null hypothesis that the number of entities
     per taxonomy category follows a uniform distribution 
 
     Args:
         tax (pd.DataFrame): taxonomy
-        tax_name (str): name of taxonomy
-        metrics (dict): dictionary to store results
 
     Returns:
         dict: dictionary with results
     """
-
+    metrics = defaultdict(int)
     for i in range(0, len(tax.columns)):
         dist = tax.iloc[:, i].value_counts()
         total_categories = len(dist)
@@ -27,7 +26,7 @@ def tax_chisq(tax: pd.DataFrame, tax_name: str, metrics: dict) -> dict:
         f_exp = list(repeat((total_entities/total_categories), times = total_categories))
         f_obs = list(dist.values)
 
-        metrics[tax_name]["level_{}".format(i+1)] = chisquare(f_obs,f_exp)[0]
+        metrics["Level_{}".format(i+1)] = chisquare(f_obs,f_exp)[0]
 
     return metrics
 
@@ -49,15 +48,15 @@ if __name__ == '__main__':
 
     if 'cooccur' in taxonomies:
         cooccurence_taxonomy = get_cooccurrence_taxonomy()
-        metrics = tax_chisq(cooccurence_taxonomy, 'cooccur', metrics)
+        metrics = tax_chisq(cooccurence_taxonomy)
     
     if 'centroids' in taxonomies:
         centroids_taxonomy = get_semantic_taxonomy(cluster_object='centroids').set_index('tag')
-        metrics = tax_chisq(centroids_taxonomy, 'centroids', metrics)
+        metrics = tax_chisq(centroids_taxonomy)
     
     if 'imbalanced' in taxonomies:
         imbalanced_taxonomy = get_semantic_taxonomy(cluster_object='kmeans_strict_imb').set_index('tag')
-        metrics = tax_chisq(imbalanced_taxonomy, 'imbalanced', metrics)
+        metrics = tax_chisq(imbalanced_taxonomy)
     
     upload_obj(
         metrics,

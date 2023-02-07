d7d45e4b85e53d631d7c745097742b550226c22d
David Ampudia <david.ampudia@nesta.org.uk>
2023-02-07 15:00:23 +0100

    fixed wrong merge

--- dap_aria_mapping/pipeline/data_collection/openalex.py
+++ dap_aria_mapping/pipeline/data_collection/openalex.py
@@ -16,29 +16,7 @@ import requests
 from metaflow import FlowSpec, S3, step, Parameter, retry, batch
 
 
-<<<<<<< HEAD
 YEARS = list(range(2007,2023))
-=======
-YEARS = [
-    2007,
-    2022,
-    2021,
-    2020,
-    2019,
-    2018,
-    2017,
-    2016,
-    2015,
-    2014,
-    2013,
-    2012,
-    2011,
-    2010,
-    2009,
-    2008,
-]
-
->>>>>>> 2428cbd (Adding processing for raw openalex data)
 API_ROOT = "https://api.openalex.org/works?filter="
 
 

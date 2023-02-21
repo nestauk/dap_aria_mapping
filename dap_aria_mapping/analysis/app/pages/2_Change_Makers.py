cc7bef83eaaafbba0e86f7dd5d1663b71e8b2df6
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2023-02-21 17:45:32 +0000

    adding chart placeholders

--- dap_aria_mapping/analysis/app/pages/2_Change_Makers.py
+++ dap_aria_mapping/analysis/app/pages/2_Change_Makers.py
@@ -5,7 +5,7 @@ from nesta_ds_utils.viz.altair import formatting
 from dap_aria_mapping import PROJECT_DIR
 formatting.setup_theme()
 
-PAGE_TITLE = "Horizon Scanner"
+PAGE_TITLE = "Change Makers"
 
 IMAGE_DIR = f"{PROJECT_DIR}/dap_aria_mapping/analysis/app/images"
 
@@ -26,7 +26,29 @@ with header1:
 with header2:       
     st.title(":red[Change Makers]")
 
+st.markdown(":red[**_Find the people and institutions with the ability to make waves within research areas_**]")
+
 overview_tab, overlaps_tab = st.tabs(["Overview", "Collaboration"])
+
+with overview_tab:
+    dropdown1, dropdown2, dropdown3 = st.columns(3)
+    with dropdown1:
+        group = st.selectbox(label = "Show me the:" , options = ["Individuals", "Research Groups", "Institutions"])
+    with dropdown2:
+        indicator = st.selectbox(label = "Who are producing research that is:", options = ["Emergent", "Disruptive", "Novel"])
+    with dropdown3:
+        topics = st.multiselect(label = "In at least one of the following topics:", options = ["Placeholder Topic1", "Placeholder Topic2"])
+
+    quad_chart, stacked_bars = st.columns(2)
+
+    with quad_chart:
+        st.subheader("Placeholder for Quad Chart")
+    with stacked_bars:
+        st.subheader("Placeholder for Stacked Bars")
+
+with overlaps_tab:
+    st.subheader("Placeholder for network")
+
 
 #adds the nesta x aria logo at the bottom of each tab, 3 lines below the contents
 st.markdown("")

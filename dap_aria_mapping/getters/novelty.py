4b346a68c0ee1f55dcdd62d886d8c82aeab6ea48
beingkk <kandersk@gmail.com>
2023-03-13 14:42:33 +0000

    pipeline for calculating patent novelty scores

--- dap_aria_mapping/getters/novelty.py
+++ dap_aria_mapping/getters/novelty.py
@@ -105,3 +105,62 @@ def get_topic_novelty_openalex(
             filepath,
             download_as="dataframe",
         )
+
+
+def get_patent_novelty_scores(level: int = 5, from_local: bool = False) -> pd.DataFrame:
+    """
+    Downloads novelty scores for patents
+
+    Args:
+        level (int): Taxonomy level that was used for novelty calculation
+        from_local (bool): Whether to load the data from local disk
+
+    Returns:
+        pd.DataFrame: Novelty scores for patents. Columns are:
+            - work_id: OpenAlex work id
+            - year: priority year
+            - commonness: commonness score for the patent
+            - novelty_score: novelty score for the patent
+            - n_topics: number of topics in the patent
+            - topics: list of topics in the patent
+
+    """
+    filepath = f"outputs/novelty/patent_novelty_{level}.parquet"
+    if from_local:
+        return pd.read_parquet(PROJECT_DIR / filepath)
+    else:
+        return download_obj(
+            BUCKET_NAME,
+            filepath,
+            download_as="dataframe",
+        )
+
+
+def get_patent_topic_pair_commonness(
+    level: int = 5, from_local: bool = False
+) -> pd.DataFrame:
+    """
+    Downloads topic pair commonness scores for patents
+
+    Args:
+        level (int): Taxonomy level that was used for commonness calculations
+        from_local (bool): Whether to load the data from local disk
+
+    Returns:
+        pd.DataFrame: Topic pair commonness scores for patents. Columns are:
+            - topic_1: first topic in the pair
+            - topic_2: second topic in the pair
+            - year: priority year
+            - N_ij_t: Number of co-occurrences of topic_1 and topic_2 in year t
+            - commonness: commonness score for the topic pair in year t
+
+    """
+    filepath = f"outputs/novelty/patent_topic_pair_commonness_{level}.parquet"
+    if from_local:
+        return pd.read_parquet(PROJECT_DIR / filepath)
+    else:
+        return download_obj(
+            BUCKET_NAME,
+            filepath,
+            download_as="dataframe",
+        )

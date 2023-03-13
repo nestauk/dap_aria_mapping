5ff21f0162c8d6fd7b6f78eb53cd63b60ae304c7
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2023-03-13 14:22:25 +0000

    fixing alignment chart

--- dap_aria_mapping/analysis/app/pages/1_Horizon_Scanner.py
+++ dap_aria_mapping/analysis/app/pages/1_Horizon_Scanner.py
@@ -22,9 +22,8 @@ st.set_page_config(
     page_icon=icon
 )
 
-#must wrap getter in another function so streamlit doesn't load the file in each time
 @st.cache_data
-def load_volume_data():
+def load_overview_data():
     volume_data = volume_per_year()
     volume_data = volume_data.with_columns(
         (pl.col('patent_count') + pl.col('publication_count')).alias('total_docs'),
@@ -32,25 +31,56 @@ def load_volume_data():
     )
     unique_domains = list(list(volume_data.select(pl.col("domain").unique()))[0])
     unique_domains.insert(0,"All")
-    return volume_data, unique_domains
 
+    alignment_data = volume_data.melt(id_vars = ["year", "topic", "area", "domain"], value_vars = ["publication_count", "patent_count"])
+    alignment_data.columns = ["year", "topic", "area", "domain", "doc_type", "count"]
+
+    return volume_data, alignment_data, unique_domains
 
 @st.cache_data
-def filter_by_domain(domain, _volume_data):
+def filter_by_domain(domain, _volume_data, _alignment_data):
     volume_data = _volume_data.filter(pl.col("domain")==domain)
+    alignment_data = _alignment_data.filter(pl.col("domain")==domain)
     unique_areas = list(list(volume_data.select(pl.col("area").unique()))[0])
-    return volume_data, unique_areas
+    return volume_data, alignment_data, unique_areas
 
 @st.cache_data
-def filter_by_area(area, _volume_data):
+def filter_by_area(area, _volume_data, _alignment_data):
     volume_data = _volume_data.filter(pl.col("area")==area)
+    alignment_data = _alignment_data.filter(pl.col("area")==area)
     unique_topics = list(list(volume_data.select(pl.col("topic").unique()))[0])
-    return volume_data, unique_topics
+    return volume_data, alignment_data, unique_topics
+
+def group_emergence_by_level(_volume_data, level, y_col):
+    q = (_volume_data.lazy().with_columns(
+        pl.col(level).cast(str)
+        ).groupby(
+            [level, "year"]
+            ).agg(
+                [pl.sum(y_col)]
+                ).filter(pl.any(pl.col("year").is_not_null())))
+    return q.collect()
 
 #@st.cache_data
-def group_by_level(_volume_data, level, sum_col):
-    q = (_volume_data.lazy().with_columns(pl.col(level).cast(str)).groupby(["year", level]).agg([pl.sum(sum_col)]).filter(pl.any(pl.col("year").is_not_null())))
+def group_alignment_by_level(_alignment_data, level):
+    total_pubs = _alignment_data.filter(pl.col("doc_type")=="publication_count").select(pl.sum("count"))
+    total_patents = _alignment_data.filter(pl.col("doc_type")=="patent_count").select(pl.sum("count"))
+    q = (_alignment_data.lazy().with_columns(
+        pl.col(level).cast(str)
+        ).groupby(["doc_type", level]
+        ).agg(
+            [pl.sum("count").alias("total")]
+        ).with_columns(
+            pl.when(pl.col("doc_type") == "publication_count")
+            .then(pl.col("total")/total_pubs)
+            .when(pl.col("doc_type") == "patent_count")
+            .then(pl.col("total")/total_patents)
+            .alias("doc_fraction")
+        ))
     return q.collect()
+
+
+
 
 #@st.cache_data
 def convert_to_pandas(_df: pl.DataFrame) -> pd.DataFrame:
@@ -65,28 +95,27 @@ with header2:
 st.markdown(f'<h1 style="color:#0000FF;font-size:16px;">{"<em>Explore patterns and trends in research domains across the UK<em>"}</h1>', unsafe_allow_html=True)
 
 #load in volume data 
-volume_data, unique_domains = load_volume_data()
+volume_data, alignment_data, unique_domains = load_overview_data()
 
-#unique_domains.insert(0, "All")
+#domain_drop, area_drop = st.columns(2)
 
-domain_drop, area_drop = st.columns(2)
-
-with domain_drop:
+with st.sidebar:
+#with domain_drop:
     domain = st.selectbox(label = "Select a Domain", options = unique_domains)
     area = "All"
     topic = "All"
     level_considered = "domain"
     if domain != "All":
-        volume_data, unique_areas = filter_by_domain(domain, volume_data)
+        volume_data, alignment_data, unique_areas = filter_by_domain(domain, volume_data, alignment_data)
         level_considered = "area"
 
     
-with area_drop:
+#with area_drop:
     if domain != "All":
         unique_areas.insert(0, "All")
         area = st.selectbox(label = "Select an Area", options = unique_areas)
         if area != "All":
-            volume_data, unique_topics = filter_by_area(area, volume_data)
+            volume_data, alignment_data, unique_topics  = filter_by_area(area, volume_data, alignment_data)
             level_considered = "topic"
 
 
@@ -95,29 +124,49 @@ with area_drop:
 overview_tab, disruption_tab, novelty_tab, overlaps_tab = st.tabs(["Overview", "Disruption", "Novelty","Overlaps"])
 
 with overview_tab:
-    volume, alignment = st.columns(2)
-    with volume:
-        st.subheader("Emergence")
-        show_only = st.selectbox(label = "Show Emergence In:", options = ["Publications", "Patents", "Both"])
-        if show_only == "Publications":
-            y_col = "publication_count"
-        elif show_only == "Patents":
-            y_col = "patent_count"
-        else:
-            y_col = "total_docs"
 
-        emergence_data = convert_to_pandas(group_by_level(volume_data, level_considered, y_col))
+    st.subheader("Growth Over Time")
+    st.markdown("View trends in volume of content over time to detect emerging or stagnant areas of innovation")
+    show_only = st.selectbox(label = "Show Emergence In:", options = ["All Documents", "Publications", "Patents"])
+    if show_only == "Publications":
+        y_col = "publication_count"
+    elif show_only == "Patents":
+        y_col = "patent_count"
+    else:
+        y_col = "total_docs"
 
-        volume_chart = alt.Chart(emergence_data).mark_line().encode(
-            alt.X("year:N"),
-            alt.Y("{}:Q".format(y_col)),
-            color = "{}:N".format(level_considered)
-        ).interactive().properties(height=600)
+    emergence_data = convert_to_pandas(group_emergence_by_level(volume_data, level_considered, y_col))
+
+    volume_chart = alt.Chart(emergence_data).mark_line().encode(
+        alt.X("year:N"),
+        alt.Y("{}:Q".format(y_col)),
+        color = "{}:N".format(level_considered)
+    ).interactive().properties(width=1000, height = 500)
     st.altair_chart(volume_chart)
 
-    with alignment:
-        st.subheader("Alignment")
-        st.markdown("This could illustrate if research is becoming more/less aligned with industry in certain areas")
+    st.subheader("Alignment in Research and Industry")
+    st.markdown("Areas with high publication count and low patent count indicates there is significantly more activity in academia than industry on this topic (or vice versa).")
+    filtered_alignment_data = convert_to_pandas(group_alignment_by_level(alignment_data, level_considered))
+    alignment_chart = alt.Chart(filtered_alignment_data).transform_filter(
+        alt.datum.doc_fraction > 0  
+        ).mark_point().encode(
+        #alt.X("{}:N".format(level_considered), title=None, axis=None),
+        alt.Y("doc_fraction:Q", 
+            title = "Fraction of Documents of the Given Type", 
+            scale=alt.Scale(type="log"), 
+            axis = alt.Axis(tickSize=0)),
+        column = alt.Column("{}:N".format(level_considered), 
+            title=None, 
+            header = alt.Header(labelFontSize=10, labelOrient = 'bottom')),
+        color = alt.Color("doc_type:N", legend=alt.Legend(
+            direction='horizontal',
+            legendX=10,
+            legendY=-80,
+            orient = 'none',
+            titleAnchor='middle',
+            title = None)
+        )).interactive().properties(width = 20)
+    st.altair_chart(alignment_chart)
 
 with disruption_tab:
     disruption_trends, disruption_drilldown = st.columns(2)

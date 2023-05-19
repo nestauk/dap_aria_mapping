17de046ada8ec4ec56cc030956fb71f7d0f11a4e
David Ampudia <david.ampudia@nesta.org.uk>
2023-05-19 13:02:56 +0200

    added missing groupby arg

--- dap_aria_mapping/analysis/app/pages/1_Horizon_Scanner.py
+++ dap_aria_mapping/analysis/app/pages/1_Horizon_Scanner.py
@@ -319,6 +319,7 @@ def get_ranked_novelty_articles(_novelty_docs: pl.DataFrame, _topic: str):
                 {
                     "document_name": pl.first(),
                     "document_year": pl.first(),
+                    "novelty": pl.first(),
                     "topic_name": pl.list("topic_name")#.apply(lambda x: ", ".join(x))
                 }
             )
